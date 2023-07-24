d84b34409c53e8df5832a8057dc7879427a8f1c0
IT1089778 <nvrnagella@gmail.com>
2023-07-24 10:27:58 +0530

    keeping availability zone to private subnets also

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "network"  {
-  source = "github.com/nvrnagella/tf-module-vpc"
-  env = var.env
-  default_vpc_id = var.default_vpc_id
+  source                = "github.com/nvrnagella/tf-module-vpc"
+  env                   = var.env
+  default_vpc_id        = var.default_vpc_id
 
-  for_each = var.vpc
-  cidr_block = each.value.cidr_block
-  public_subnets_cidr = each.value.public_subnets_cidr
-  private_subnets_cidr = each.value.private_subnets_cidr
-  availability_zones = each.value.availability_zones
+  for_each              = var.vpc
+  cidr_block            = each.value.cidr_block
+  public_subnets_cidr   = each.value.public_subnets_cidr
+  private_subnets_cidr  = each.value.private_subnets_cidr
+  availability_zones    = each.value.availability_zones
 }
