f7875179feb85b36d6f6861d265c2c0826557ead
IT1089778 <nvrnagella@gmail.com>
2023-07-24 07:13:26 +0530

    giving availability zone

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ module "network"  {
   cidr_block = each.value.cidr_block
   public_subnets_cidr = each.value.public_subnets_cidr
   private_subnets_cidr = each.value.private_subnets_cidr
+  availability_zones = each.value.availability_zones
 }
 provider "aws" {
   region = "us-east-1"
