de59b5b68eca443806da5ea8b364633edbd1ef86
IT1089778 <nvrnagella@gmail.com>
2023-07-26 11:21:53 +0530

    included all subnets to vpc

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-module "network"  {
+module "vpc"  {
   source                = "github.com/nvrnagella/tf-module-vpc"
   env                   = var.env
   default_vpc_id        = var.default_vpc_id
 
   for_each              = var.vpc
   cidr_block            = each.value.cidr_block
-  public_subnets_cidr   = each.value.public_subnets_cidr
-  private_subnets_cidr  = each.value.private_subnets_cidr
+  subnets               = each.value.subnets
   availability_zones    = each.value.availability_zones
 }
